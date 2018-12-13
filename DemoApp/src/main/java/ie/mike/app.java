fc9c216c2ba65efa241968f6b6a8df71e1323092
Mike330320 <mikejcushen@gmail.com>
2018-12-13 14:57:14 +0000

    Database Connectors

---
+++ DemoApp/src/main/java/ie/mike/app.java
@@ -0,0 +1,30 @@
+package ie.mike;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    String connectionString = "Server=tcp:scom673.database.windows.net,1433;Initial Catalog=DCOM673;Persist Security Info=False;User ID={Mike};Password={Fbs@330320};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+	// Create the connection object
+	Connection connection = null;  
+
+	try 
+	{  
+	    // Attempt to connect to the database
+	    System.out.println("Attempting to connect to the database...");
+	    connection = DriverManager.getConnection(connectionString);  
+	    // Success!
+	    System.out.println("Connected...");
+	}
+	catch(Exception e)
+	{
+	    // Not success!
+	    System.out.println(e.getMessage());
+	}
+    }
+}
