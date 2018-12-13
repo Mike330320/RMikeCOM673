34b494843e38970a46187506724a3635a17daac8
Mike330320 <mikejcushen@gmail.com>
2018-12-13 17:48:20 +0000

    DB Success

--- DemoApp/src/main/java/ie/mike/app.java
+++ DemoApp/src/main/java/ie/mike/app.java
@@ -19,7 +19,22 @@ public class app
 	    // Attempt to connect to the database
 	    System.out.println("Attempting to connect to the database...");
 	    connection = DriverManager.getConnection(connectionString);  
-	    // Success!
+		// Success!
+		// Execute a query against the database and return rows (if any) to the ResultSet
+ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM customerTable");
+// The ResultSet places the current row directly before the first row. Calling next() moves it to the next row, and returns true if there is a row. False if there is no row there.
+while(rs.next())
+{              
+	if(rs.getBoolean("paid") == true) // If the current row has true for the boolean column 'paid'
+	{  
+    		// Print out a message made up from the columns in the database 
+    		System.out.println(rs.getString("first_name") + " " + rs.getString("last_name") + " has paid " + rs.getDouble("amount"));
+	}
+	else
+	{
+    		System.out.println(rs.getString("first_name") + " " + rs.getString("last_name") + " has not paid " + rs.getDouble("amount"));
+	}
+}
 	    System.out.println("Connected...");
 	}
 	catch(Exception e)
@@ -27,5 +42,7 @@ public class app
 	    // Not success!
 	    System.out.println(e.getMessage());
 	}
-    }
+	}
 }
+
+
