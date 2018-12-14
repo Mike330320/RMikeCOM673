f8f023a4829b04596b9886a516ac03df5d03e84a
Mike330320 <mikejcushen@gmail.com>
2018-12-14 15:46:14 +0000

    Final

---
+++ DemoVaadinApp/src/main/java/ie/mike/MyUI.java
@@ -0,0 +1,142 @@
+package ie.mike;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+
+import javax.servlet.annotation.WebServlet;
+
+
+
+import com.vaadin.annotations.Theme;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+
+import com.vaadin.server.VaadinRequest;
+
+import com.vaadin.server.VaadinServlet;
+
+import com.vaadin.ui.Button;
+
+import com.vaadin.ui.Label;
+
+import com.vaadin.ui.TextField;
+
+import com.vaadin.ui.UI;
+
+import com.vaadin.ui.Grid;
+
+import com.vaadin.ui.VerticalLayout;
+
+
+
+/**
+
+ * This UI is the application entry point. A UI may either represent a browser window 
+
+ * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+
+ * <p>
+
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+
+ * overridden to add component to the user interface and initialize non-component functionality.
+
+ */
+
+@Theme("mytheme")
+
+public class MyUI extends UI {
+
+    Connection connection = null;
+
+    @Override
+
+    protected void init(VaadinRequest vaadinRequest) {
+
+        final VerticalLayout layout = new VerticalLayout();
+
+        
+
+        // Connect to database (need Azure conn strings)
+
+        String connectionString = "jdbc:sqlserver://scom673.database.windows.net:1433;database=DCOM673;user=Mike@scom673;password={Fbs@330320};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+
+
+        try 
+
+        {
+
+            // Connect with JDBC driver to a database
+
+            connection = DriverManager.getConnection(connectionString);
+
+            ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM customerTable;");
+
+            // Convert the resultset that comes back into a List - we need a Java class to represent the data (Customer.java in this case)
+
+            List<Customer> customers = new ArrayList<Customer>();
+
+            // While there are more records in the resultset
+
+            while(rs.next())
+
+            {   
+
+                // Add a new Customer instantiated with the fields from the record (that we want, we might not want all the fields)
+
+                customers.add(new Customer(rs.getString("first_name"), 
+
+                                           rs.getString("last_name"), 
+
+                                           rs.getBoolean("paid"), 
+
+                                           rs.getDouble("amount")));
+
+            }
+            //Grid
+            Grid<Customer> myGrid=new Grid<>();
+            myGrid.setItems(customers);
+            myGrid.addColumn(Customer::getFirst_name).setCaption("Name");
+
+            layout.addComponent(myGrid);
+
+            
+
+        } 
+
+        catch (Exception e) 
+
+        {
+
+            // This will show an error message if something went wrong
+
+            layout.addComponent(new Label(e.getMessage()));
+
+        }
+
+        
+
+        setContent(layout);
+
+    }
+
+
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+
+    public static class MyUIServlet extends VaadinServlet {
+
+    }
+
+}
---
+++ VaadinJDBCApp/src/main/java/ie/mike/App.java
@@ -0,0 +1,13 @@
+package ie.mike;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
